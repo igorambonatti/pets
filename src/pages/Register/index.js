470c6611f76c0c3f742ff82640806445f4cd284a
Igor ambonatti <contatoambonatti@gmail.com>
2020-01-29 17:35:15 -0300

    ImgInput

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header/index';
 
-import { Container, Content } from './styles';
+import { Container, Content, ImgInput } from './styles';
 
 export default function Register() {
   return (
@@ -9,7 +9,15 @@ export default function Register() {
       <Header />
       <Content>
         <form>
-          <input type="file" />
+          <ImgInput>
+            <label htmlFor="avatar">
+              <img
+                src="https://api.adorable.io/avatars/285/abott@adorable.png"
+                alt=""
+              />
+              <input type="file" id="avatar" accept="image/*" />
+            </label>
+          </ImgInput>
           <input placeholder="Tipo" />
           <input placeholder="Nome" />
           <input placeholder="Descrição" />
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,9 +1,32 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+export const ImgInput = styled.div`
+  align-self: center;
+  margin-bottom: 30px;
+
+  label {
+    cursor: pointer;
+  }
+  &:hover {
+    opacity: 0.7;
+  }
+
+  img {
+    height: 180px;
+    width: 180px;
+    border-radius: 50%;
+    border: 3px solid rgba(255, 255, 255, 0.3);
+    background: none;
+  }
+
+  input {
+    display: none;
+  }
+`;
 
 export const Content = styled.div`
-margin: 50px auto;
+  margin: 50px auto;
   max-width: 500px;
   text-align: center;
   form {
@@ -11,15 +34,16 @@ margin: 50px auto;
     flex-direction: column;
     margin-top: 30px;
 
-  input{
-    height:58px;
-    background: rgba(250, 250, 250, 1);
-    margin: 0 0 10px;
-    border: 0px;
-    border-radius: 4px;
-    padding: 0 15px;
-    color: #000000;
-    font-size: 14px;
-    opacity: 0.85;
+    input {
+      height: 58px;
+      background: rgba(250, 250, 250, 1);
+      margin: 0 0 10px;
+      border: 0px;
+      border-radius: 4px;
+      padding: 0 15px;
+      color: #000000;
+      font-size: 14px;
+      opacity: 0.85;
+    }
   }
 `;
